c0b7c2665b6cfb073c4c0f3776e49e0a66b8cbe5
daikinity <dai.kon1006@gmail.com>
2019-06-13 11:16:05 +0900

    0613 ProductBean作成

---
+++ ichiban/la/bean/ProductBean.java
@@ -0,0 +1,71 @@
+package la.bean;
+
+public class ProductBean {
+	private String code;
+	private String category_code;
+	private String name;
+	private int price;
+	private String unit;
+
+	public ProductBean(
+			String code,
+			String category_code,
+			String name,
+			int price,
+			String unit) {
+
+		this.code = code;
+		this.category_code = category_code;
+		this.name = name;
+		this.price = price;
+		this.unit = unit;
+
+	}
+
+	public ProductBean() {
+
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getCategory_Code() {
+		return category_code;
+	}
+
+	public void setCategory_Code(String category_code) {
+		this.category_code = category_code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+
+
+}
