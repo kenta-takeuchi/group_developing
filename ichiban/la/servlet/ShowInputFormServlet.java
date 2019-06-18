564ed8c2ab28a441712bf5f5c3ff023a1ccb1ec3
daikinity <dai.kon1006@gmail.com>
2019-06-18 14:03:47 +0900

    登録機能DB接続（途中）

--- ichiban/la/servlet/ShowInputFormServlet.java
+++ ichiban/la/servlet/ShowInputFormServlet.java
@@ -1,8 +1,8 @@
 package la.servlet;
 
 import java.io.IOException;
-
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,6 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import la.bean.OrderDetailBean;
+import la.bean.ProductBean;
+import la.dao.DataAccessException;
+import la.dao.PostgreSQLProductDao;
 
 @WebServlet("/ShowInputFormServlet")
 public class ShowInputFormServlet extends HttpServlet {
@@ -28,18 +31,43 @@ public class ShowInputFormServlet extends HttpServlet {
  					request.setAttribute("customer_code", customer_code);
  					// 次のサーブレットに渡すリストを準備
  					ArrayList<OrderDetailBean> list= new ArrayList<OrderDetailBean>();
+
  					for (int cnt = 1; cnt < 11; cnt++) {
  						// 前画面から渡された１明細ごとにBeanを作成し、リストに追加
  	 					OrderDetailBean bean = new OrderDetailBean();
- 	 					if (request.getParameter("order_id" + cnt).length() > 0) {
- 	 	 					bean.setOrder_id(request.getParameter("order_id" + cnt));
- 	 	 					bean.setQuantity(Integer.parseInt(request.getParameter("quantity" + cnt)));
+ 	 					if (request.getParameter("quantity_" + cnt).length() > 0) {
+ 	 	 					bean.setOrder_id(request.getParameter("product_" + cnt));
+ 	 	 					bean.setQuantity(Integer.parseInt(request.getParameter("quantity_" + cnt)));
  	 	 					list.add(bean);
  	 					}
  					}
+
+ 					PostgreSQLProductDao productDao = null;
+ 					List<ProductBean> products = null;
+					try {
+						productDao = new PostgreSQLProductDao();
+						products = productDao.selectAll();
+					} catch (DataAccessException e) {
+						// TODO 自動生成された catch ブロック
+						e.printStackTrace();
+					}
+
+ 					request.setAttribute("products", products);
 					request.setAttribute("list", list);
 					gotoPage(request, response, "/OrderRegistConfirm.jsp");
-					
+
+				}else if(action.equals("list")) {
+ 					PostgreSQLProductDao productDao = null;
+ 					List<ProductBean> products = null;
+					try {
+						productDao = new PostgreSQLProductDao();
+						products = productDao.selectAll();
+					} catch (DataAccessException e) {
+						// TODO 自動生成された catch ブロック
+						e.printStackTrace();
+					}
+ 					request.setAttribute("products", products);
+					gotoPage(request, response, "/OrderRegist.jsp");
 				}else {
 					request.setAttribute("message", "正しく操作してください");
 					gotoPage(request, response, "/OrderRegistError.jsp");
